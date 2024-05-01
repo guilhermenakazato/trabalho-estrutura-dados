e2a240929810ea66e96e5a446b43d77b4ca4b076
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2024-04-30 23:15:49 -0400

    [NEW] Reworking parser...

--- parser.c
+++ parser.c
@@ -1,54 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <locale.h>
-#include <wchar.h>
 
 int readFile(char * filePath) {
     FILE* file;
     file = fopen(filePath, "r");
-    wchar_t ch;
+    char line[50];
+    char attributes[][30] = {"codigo_ibge", "nome", "latitude",
+                             "longitude", "capital", "codigo_uf",
+                             "siafi_id", "ddd", "fuso_horario"};
 
     if(file == NULL) {
         printf("Não foi possível abrir o arquivo");
     } else {
-        int brokeLine = 0;
-        int shouldSplit = 1;
-        int printedCharBefore = 0;
-        int counter = 1;
+        int codigo_ibge, capital, codigo_uf, siafi_id, ddd;
+        float latitude, longitude; 
+        char nome[50], fuso_horario[20], fuso_horario2[30];
 
-        while(ch != EOF) {
-            ch = fgetwc(file);
-            
-            if(ch >= 45 && ch <= 57
-            || ch >= 65 && ch <= 90
-            || ch >= 95 && ch <= 122
-            || ch >= 192 && ch <= 250) {
-                brokeLine = 0;      
-                printedCharBefore = 1;     
-
-                if(counter % 2 == 0)
-                    printf("%lc", ch);
-            }
-
-            if(ch == 32) {
-                if(shouldSplit && !brokeLine) {
-                    counter++;
-                    shouldSplit = 0;
-                } else if(!brokeLine && !shouldSplit) {
-                    printf("%c", ch);
-                }
-            }
-
-            if(ch == 10) {
-                if(printedCharBefore) { 
-                    printf("%c", ch);
-                    counter++;
-                }
-
-                brokeLine = 1;
-                shouldSplit = 1;
-                printedCharBefore = 0;
+        while(fgets(line, 50, file) != NULL) {
+            if(strstr(line, attributes[0]) != NULL) {
+                sscanf(line, "%*s %d", &codigo_ibge);
+                printf("%d\n", codigo_ibge);
+            } else if(strstr(line, attributes[1]) != NULL) {
+                sscanf(line, "%*s \"%[^\"]", nome);
+                printf("%s\n", nome);
+            } else if(strstr(line, attributes[2]) != NULL) {
+                sscanf(line, "%*s %f", &latitude);
+                printf("%f\n", latitude);
+            } else if(strstr(line, attributes[3]) != NULL) {
+                sscanf(line, "%*s %f", &longitude);
+                printf("%f\n", longitude);
+            } else if(strstr(line, attributes[4]) != NULL) {
+                sscanf(line, "%*s %d", &capital);
+                printf("%d\n", capital);
+            } else if(strstr(line, attributes[5]) != NULL) {
+                sscanf(line, "%*s %d", &codigo_uf);
+                printf("%d\n", codigo_uf);
+            } else if(strstr(line, attributes[6]) != NULL) {
+                sscanf(line, "%*s %d", &siafi_id);
+                printf("%d\n", siafi_id);
+            } else if(strstr(line, attributes[7]) != NULL) {
+                sscanf(line, "%*s %d", &ddd);
+                printf("%d\n", ddd);
+            } else if(strstr(line, attributes[8]) != NULL) {
+                sscanf(line, "%*s \"America\\%[^\"]", fuso_horario2);
+                strcpy(fuso_horario, "America");
+                strcat(fuso_horario, fuso_horario2);
+                printf("%s\n", fuso_horario);
             }
         }
     }
@@ -57,7 +55,6 @@ int readFile(char * filePath) {
 }
 
 int main() {
-    setlocale(LC_CTYPE, "");
     readFile("municipios.json");
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS;  
 } 
